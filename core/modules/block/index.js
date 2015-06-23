88c2d77cc1c0158faf2fd1074ba4e9b4b030673f
Antonio Manuel Pérez López <aperezl@gmail.com>
2015-06-23 10:55:39 +0200

    Added block system

--- core/modules/block/index.js
+++ core/modules/block/index.js
@@ -11,6 +11,22 @@ module.exports = function(unuko) {
 		console.log('gestion de bloques');
 	};
 
+	module.router = function() {
+		return [
+			{
+				title: 'get block',
+				method: 'get',
+				url: '/getblock/:blockId',
+				menu: 'get block',
+				action: function(req, res) {
+					res.render('blocks/' + req.params.blockId, {}, function(err, html) {
+						res.send({html: html});
+					});
+				}
+			}
+		]
+	};
+
 	return module;
 
 }
